e54dcd5684c7561a315024e9a78788cffc0856af
AnnaP-G <klientkv1511@gmail.com>
2024-05-26 22:04:48 +0300

    hw2server

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { env } from './utils/env.js';
 import { ENV_VARS } from './constants/constants.js';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js';
 import { notFoundMiddleware } from './middlewares/notFoundMiddleware.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 export const setupServer = () => {
   const app = express();
@@ -12,8 +13,39 @@ export const setupServer = () => {
   app.use(pino({ transport: { target: 'pino-pretty' } }));
   app.use(cors());
 
-  app.get('/contacts', (req, res, next) => {
-    res.send('hello world');
+  app.get('/contacts', async (req, res, next) => {
+    try {
+      const contacts = await getAllContacts();
+      res.json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    try {
+      const contactId = req.params.contactId;
+      const contact = await getContactById(contactId);
+
+      if (!contact) {
+        return res.status(404).json({
+          status: 404,
+          message: `Contact with id ${contactId} not found!`,
+        });
+      } else {
+        return res.json({
+          status: 200,
+          message: `Successfully found contact with id ${contactId}!`,
+          data: contact,
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
   });
 
   app.use('*', notFoundMiddleware);
